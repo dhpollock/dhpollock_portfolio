7fa98bb422ed2305a4d301597390feb9095d651a
dhpollock <harmon@dhpollock.com>
2020-04-13 10:00:22 -0700

    Updates

---
+++ precache-manifest.36c8ddf3ed528e22040b2cab05c40bcd.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e06d4f2ab61e961c5bc67c508441ffb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0154f428bd115b863d8e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7a954059f6d01002e78f",
+    "url": "/static/js/2.9dc3e372.chunk.js"
+  },
+  {
+    "revision": "4ae5b0d8993c7a7586cb214d7f639d47",
+    "url": "/static/js/2.9dc3e372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0154f428bd115b863d8e",
+    "url": "/static/js/main.424f9b95.chunk.js"
+  },
+  {
+    "revision": "99c5e18b3565ca73aaaf",
+    "url": "/static/js/runtime-main.6729433d.js"
+  },
+  {
+    "revision": "a0f206eafc9def57cc3df8992dbfcfdf",
+    "url": "/static/media/US10140749.a0f206ea.pdf"
+  },
+  {
+    "revision": "03636a7389f680cb828fbe524cb5095c",
+    "url": "/static/media/banner2.03636a73.jpg"
+  },
+  {
+    "revision": "c16a702c1d0bc9c4e09f337b4fec011c",
+    "url": "/static/media/cookie.c16a702c.jpg"
+  },
+  {
+    "revision": "254fc9a29e8486751effa1a1070d5e66",
+    "url": "/static/media/deadspaceUI.254fc9a2.png"
+  },
+  {
+    "revision": "a52d0dcd5c8ab0ceaad20d32c00a015b",
+    "url": "/static/media/desktop01.a52d0dcd.jpg"
+  },
+  {
+    "revision": "a3f0bc1ed6796940a80106891a7462f2",
+    "url": "/static/media/desktop02.a3f0bc1e.jpg"
+  },
+  {
+    "revision": "d3d9c962ca3ad61f20a91fca47f9c228",
+    "url": "/static/media/desktop03.d3d9c962.jpg"
+  },
+  {
+    "revision": "118c1ddec0ab0fd10cd40a110b70ae3d",
+    "url": "/static/media/dhpollock_bike.118c1dde.png"
+  },
+  {
+    "revision": "6d2b97f2a1e5612aa8202f278e14d212",
+    "url": "/static/media/emberlast_sticky.6d2b97f2.png"
+  },
+  {
+    "revision": "ad8f879bf685816e32ec32eb07cf50c3",
+    "url": "/static/media/final01.ad8f879b.jpg"
+  },
+  {
+    "revision": "1efef7776d66ee80b9e4012b8401db33",
+    "url": "/static/media/final02.1efef777.jpg"
+  },
+  {
+    "revision": "11f86f175074d46564ec8beed5109a84",
+    "url": "/static/media/final03.11f86f17.jpg"
+  },
+  {
+    "revision": "292cd1d61ae9c7c0efa5e3ff8efd0fb5",
+    "url": "/static/media/harmon_pollock.292cd1d6.jpg"
+  },
+  {
+    "revision": "768c4c2ab299e3360861a731883b1f74",
+    "url": "/static/media/hpe_demolvl01.768c4c2a.png"
+  },
+  {
+    "revision": "6d67260f4b28edaedda2303de4d9c7be",
+    "url": "/static/media/hpe_demolvl02.6d67260f.png"
+  },
+  {
+    "revision": "8cc8c77f07e24f1c1abe8f0d01ed7d7a",
+    "url": "/static/media/hpe_demolvl03.8cc8c77f.png"
+  },
+  {
+    "revision": "794f511b044e9966bd60be3e352d77ae",
+    "url": "/static/media/hpe_demolvl04.794f511b.png"
+  },
+  {
+    "revision": "2c5d8dd5c49fc33a3360ab35bbe670c5",
+    "url": "/static/media/hpe_demolvl05.2c5d8dd5.png"
+  },
+  {
+    "revision": "377e10d23cc5d57ab0f371acaea16203",
+    "url": "/static/media/hpe_demolvl06.377e10d2.png"
+  },
+  {
+    "revision": "017f2d96da9d4660abdea865b81453ff",
+    "url": "/static/media/mobile01.017f2d96.jpg"
+  },
+  {
+    "revision": "87e13b4e7221d2179a6d4362b6b43c02",
+    "url": "/static/media/mobile02.87e13b4e.jpg"
+  },
+  {
+    "revision": "098b77236bd4ecce6161620296621efd",
+    "url": "/static/media/pdf_icon.098b7723.png"
+  },
+  {
+    "revision": "1506b3be1db85472a377dcff7a279121",
+    "url": "/static/media/persona.1506b3be.png"
+  },
+  {
+    "revision": "05f47f3e07a19505b2d6a7c95e4b6392",
+    "url": "/static/media/pollock_harmon_webresume.05f47f3e.pdf"
+  },
+  {
+    "revision": "b5dd50e7ae0da7a24512c563690343a6",
+    "url": "/static/media/research.b5dd50e7.png"
+  },
+  {
+    "revision": "0fe6a5c994c8d3b74c675e52d74a6f08",
+    "url": "/static/media/sticky.0fe6a5c9.png"
+  },
+  {
+    "revision": "66485396ec21ca3637410ded1ec12774",
+    "url": "/static/media/users.66485396.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dhpollock_portfolio/precache-manifest.afb2ee8ac04570bce89731f2a8a3280d.js"
+  "/precache-manifest.36c8ddf3ed528e22040b2cab05c40bcd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dhpollock_portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
